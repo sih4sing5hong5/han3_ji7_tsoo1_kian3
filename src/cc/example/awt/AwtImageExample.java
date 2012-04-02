82ad14cdc857ab0dde09ccdfe2b2583d5cf726cf
Ihc <ihcaoe@gmail.com>
2012-04-02 05:42:58 +0000

    備份一下範例 by Ihc

---
+++ src/cc/example/awt/AwtImageExample.java
@@ -0,0 +1,111 @@
+package cc.example.awt;
+
+/*
+ * Copyright (c) 2000 David Flanagan.  All rights reserved.
+ * This code is from the book Java Examples in a Nutshell, 2nd Edition.
+ * It is provided AS-IS, WITHOUT ANY WARRANTY either expressed or implied.
+ * You may study, use, and modify it for any non-commercial purpose.
+ * You may distribute it non-commercially as long as you retain this notice.
+ * For a commercial use license, or to purchase the book (recommended),
+ * visit http://www.davidflanagan.com/javaexamples2.
+ */
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import cc.adjusting.image.SimpleImageAdjuster;
+import cc.core.ChineseCharacter;
+import cc.core.ChineseCharacterUtility;
+import cc.moveable_type.ChineseCharacterMovableType;
+import cc.moveable_type.image.ImageMoveableType;
+import cc.printing.awt.AwtForImagePrinter;
+import cc.setting.ChineseCharacterTypeSetter;
+import cc.setting.image.SimpleImageSetter;
+
+/** A demonstration of writing custom Stroke classes */
+public class AwtImageExample extends JPanel {
+	private static final long serialVersionUID = 1L;
+	static final int WIDTH = 1024, HEIGHT = 800; // Size of our example
+	static final int TYPE_SIZE = 200;
+	private String word = "秋漿國⿰禾火⿱將水⿴囗或⿱⿰⿰糹言糹攵⿰矛⿱攵力⿱木⿰木木變務森攵力木";
+	private String FontName = "全字庫正宋體";
+	private int FontStyle = Font.BOLD;
+
+	public String getName() {
+		return "Custom Strokes";
+	}
+
+	public int getWidth() {
+		return WIDTH;
+	}
+
+	public int getHeight() {
+		return HEIGHT;
+	}
+
+	Stroke[] strokes = new Stroke[] { new BasicStroke(4.0f), // The standard,
+	// predefined
+	// stroke
+	};
+
+	/** Draw the example */
+	public void paint(Graphics g1) {
+		Graphics2D g = (Graphics2D) g1;
+		// Font f = new Font(FontName, FontStyle, 140);
+		// Set drawing attributes and starting position
+		g.setColor(Color.black);
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g.translate(WIDTH - TYPE_SIZE - 20, 0);
+		g.setStroke(new NullStroke());
+
+		ChineseCharacterUtility ccUtility = new ChineseCharacterUtility(word);
+		Vector<ChineseCharacter> ccArray = ccUtility.parseText();
+		ChineseCharacterTypeSetter writer = new SimpleImageSetter();
+		Vector<ChineseCharacterMovableType> ccmvArray = new Vector<ChineseCharacterMovableType>();
+		for (int i = 0; i < ccArray.size(); ++i) {
+			ccmvArray.add(ccArray.elementAt(i).typeset(writer));
+		}
+		Point model = new Point(TYPE_SIZE, TYPE_SIZE);
+		SimpleImageAdjuster sampleImageAdjuster = new SimpleImageAdjuster();
+		for (int i = 0; i < ccArray.size(); ++i) {
+			((ImageMoveableType) ccmvArray.elementAt(i)).setRegion(model);
+			ccmvArray.elementAt(i).adjust(sampleImageAdjuster);
+		}
+		System.out.println(ccArray.size());
+		AwtForImagePrinter awtForImagePrinter = new AwtForImagePrinter(g,
+				FontName, FontStyle);
+		for (int i = 0; i < ccmvArray.size(); ++i) {
+			ccmvArray.elementAt(i).print(awtForImagePrinter);
+			g.translate(0, TYPE_SIZE);// move to the down
+			if (i % 3 == 2)
+				g.translate(-TYPE_SIZE * 1, -TYPE_SIZE * 3);// the new line
+		}
+		return;
+	}
+
+	public static void main(String[] a) {
+		JFrame f = new JFrame();
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		f.setContentPane(new AwtImageExample());
+		f.setSize(WIDTH, HEIGHT);
+		f.setVisible(true);
+	}
+
+}

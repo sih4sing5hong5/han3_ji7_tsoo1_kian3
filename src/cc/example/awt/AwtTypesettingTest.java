2a4bf65f4cbe182cbc87921f8e383efefa697eb3
Ihc <ihcaoe@gmail.com>
2012-08-01 11:40:11 +0000

    改變測資 by Ihc

--- src/cc/example/awt/AwtTypesettingTest.java
+++ src/cc/example/awt/AwtTypesettingTest.java
@@ -14,31 +14,16 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.font.FontRenderContext;
 import java.awt.font.GlyphVector;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
-import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
-import java.util.Vector;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import cc.adjusting.ChineseCharacterTypeAdjuster;
 import cc.adjusting.piece.ShapeInformation;
-import cc.adjusting.piece.SimplePieceAdjuster;
-import cc.core.ChineseCharacter;
-import cc.core.ChineseCharacterUtility;
-import cc.moveable_type.ChineseCharacterMovableType;
-import cc.moveable_type.piece.PieceMovableType;
-import cc.printing.ChineseCharacterTypePrinter;
-import cc.printing.awt.piece.AwtForPiecePrinter;
-import cc.setting.ChineseCharacterTypeSetter;
-import cc.setting.piece.SimplePieceSetter;
 
 /** A demonstration of writing custom Stroke classes */
 public class AwtTypesettingTest extends JPanel
@@ -47,14 +32,13 @@ public class AwtTypesettingTest extends JPanel
 	static final int WIDTH = 1420, HEIGHT = 1050; // Size of our example
 	static final int TYPE_SIZE = 200;
 	static final int LINE_SIZE = 4;
-	private String word = /* "⿰禾火秋"; // */"秋漿國一" + "⿰禾火⿱將水⿴囗或二"
-			+ "⿱⿰⿰糹言糹攵⿰矛⿱攵力⿱木⿰木木三" + "變務森四" + "攵力木五";// */;
-	static final String SUNG_FONT = "全字庫正宋體";
-	static final String KAI_FONT = "全字庫正楷體";
-	static final String BLACK_FONT = "文泉驛正黑";
+	String word = /* "⿰禾火秋"; // */"秋漿國一" + "⿰禾火⿱將水⿴囗或二" + "⿱⿰⿰糹言糹攵⿰矛⿱攵力⿱木⿰木木三"
+			+ "變務森四" + "攵力木五";// */;
+	static final String 全字庫正宋體 = "全字庫正宋體";
+	static final String 全字庫正楷體 = "全字庫正楷體";
+	static final String 文泉驛正黑 = "文泉驛正黑";
 	static final String 文鼎中圓 = "文鼎中圓";
-	static private final String FontName = SUNG_FONT;
-	private int FontStyle = Font.BOLD;
+	static final String 超研澤中圓 = "超研澤中圓";
 
 	public String getName()
 	{
@@ -76,15 +60,15 @@ public class AwtTypesettingTest extends JPanel
 	{
 		Graphics2D g = (Graphics2D) g1;
 		final int TEST = 144;
-		Font f = new Font("全字庫正宋體", Font.BOLD, TEST);
+		Font f = new Font(文泉驛正黑, Font.BOLD, TEST);
 		GlyphVector gv = f
 				.createGlyphVector(((Graphics2D) g1).getFontRenderContext(),
-						"一二三變務森國a,龜龘"/* ⿻ab!" */);
+						"一二三變⿴務⿰意⿱國a,龜龘"/* ⿻ab!" */);
 		// System.out.println(gv.getNumGlyphs());
 		// Shape shape = gv.getGlyphLogicalBounds(0);
 		System.out.println(gv.getGlyphLogicalBounds(0));
 		System.out.println(gv.getGlyphOutline(0).getBounds());
-		Rectangle2D.Double lDouble = new Rectangle2D.Double(0, 0, 1000,1 );
+		Rectangle2D.Double lDouble = new Rectangle2D.Double(0, 0, 1000, 1);
 
 		g.setStroke(new NullStroke());
 		g.translate(0, 144);
@@ -105,8 +89,9 @@ public class AwtTypesettingTest extends JPanel
 			System.out.println((new Area(gv.getGlyphVisualBounds(i)))
 					.getBounds2D());
 			System.out.println((new Area(gv.getGlyphVisualBounds(i)))
-					.getBounds2D().getY()+(new Area(gv.getGlyphVisualBounds(i)))
-					.getBounds2D().getHeight()/2);
+					.getBounds2D().getY()
+					+ (new Area(gv.getGlyphVisualBounds(i))).getBounds2D()
+							.getHeight() / 2);
 			// System.out.println(gv.getGlyphPosition(i));
 			ShapeInformation shapeInformation = new ShapeInformation(new Area(
 					gv.getGlyphOutline(i)));

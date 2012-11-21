1d230633b246d213a56d5fad8b53b359adb2b799
Ihc <ihcaoe@gmail.com>
2012-11-21 13:28:09 +0000

    已可將構形資料中的連接及方便符號移植到漢字組建 丞宏

--- src/cc/tool/database/構形資料庫轉至組建資料庫.java
+++ src/cc/tool/database/構形資料庫轉至組建資料庫.java
@@ -20,8 +20,10 @@ public class 構形資料庫轉至組建資料庫
 	 */
 	public static void main(String[] args)
 	{
+		System.out.println("開始嘍～～ 時間：" + System.currentTimeMillis());
 		PgsqlConnection 選取連線 = new PgsqlConnection();
 		PgsqlConnection 更新連線 = new PgsqlConnection();
+		構形資料庫轉至組建資料庫 轉移工具 = new 構形資料庫轉至組建資料庫();
 		int 上傳筆數 = 0;
 		try
 		{
@@ -29,77 +31,65 @@ public class 構形資料庫轉至組建資料庫
 			String selectQuery = "SELECT \"編號\",\"Big5\",\"Unicode\",\"連接符號\",\"部件序\""
 					+ " FROM \"構形資料庫\".\"檢字表\" "
 					// + " WHERE \"連接符號\" = '5' "
-					+ " ORDER BY \"Unicode\" ASC, \"Big5\" ASC" /*
-																 * +
-																 * " LIMIT 100"
-																 */;
+					+ " ORDER BY \"Unicode\" ASC, \"Big5\" ASC"
+			// + " LIMIT 100"
+			;
 			ResultSet rs = 選取連線.executeQuery(selectQuery);
-			Vector<String> file = new Vector<String>();
 			while (rs.next())
 			{
-				// int 字體 = Integer.parseInt(rs.getString("字體"));
-				// 字體 <<= 16;
-				// int[] 字碼 = 字串與控制碼轉換.轉換成控制碼(rs.getString("字碼"));// TODO
-				// if (字碼.length == 1)
-				// 字體 += 字碼[0];
-				// else
-				// 字體 = 0;
-				// System.out.println(字碼.length + " " + 字碼[0]);
-
 				int 連接符號 = Integer.parseInt(rs.getString("連接符號"));
 				String 部件序 = rs.getString("部件序");
 				int[] 部件序控制碼 = 字串與控制碼轉換.轉換成控制碼(部件序);
-				if (1 <= 連接符號 && 連接符號 <= 3)
+				String 構字符號 = null;
+				switch (連接符號)
 				{
-					if (部件序控制碼.length == 2)
+				case 1:
+					構字符號 = "⿰";
+					break;
+				case 2:
+					構字符號 = "⿱";
+					break;
+				case 3:
+					構字符號 = "⿴";
+					break;
+				default:
+					break;
+				}
+				try
+				{
+					switch (連接符號)
 					{
-						String 構字符號 = new String();
-						switch (連接符號)
-						{
-						case 1:
-							構字符號 = "⿱";
-							break;
-						case 2:
-							構字符號 = "⿰";
-							break;
-						case 3:
-							構字符號 = "⿴";
-							break;
-						default:
-							break;
-						}
+					case 1:
+					case 2:
+					case 3:
+					case 5:
 						String insertQuery = "INSERT INTO \"漢字組建\".\"檢字表\" "
 								+ "(\"Unicode\",\"Big5\",\"構形資料庫編號\",\"構字式\") "
 								+ " VALUES ('" + rs.getString("Unicode")
 								+ "','" + rs.getString("Big5") + "','"
-								+ rs.getString("編號") + "','" + 部件序 + "')";
+								+ rs.getString("編號") + "','"
+								+ 轉移工具.產生構字式(部件序控制碼, 構字符號) + "')";
 						更新連線.executeUpdate(insertQuery);
 						上傳筆數++;
-					}
-					else
-					{
-						// TODO
-						System.out.println(部件序 + " " + rs.getString("編號"));
+						break;
+					default:
+						break;
+
 					}
 				}
-				else if (連接符號 == 5)
+				catch (IOException e)
 				{
-					if (部件序控制碼.length != 2)
-						System.out.println("有問題！！ " + 部件序);
-					else
-						上傳筆數++;
-				}
-				else
-				{
-					// TODO
+					System.err.println("發現錯誤！！！ " + rs.getString("編號") + " "
+							+ 部件序);
+					e.printStackTrace();
 				}
 			}
 		}
 		catch (SQLException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("結束嘍～～ 時間：" + System.currentTimeMillis());
 		System.out.println("上傳筆數=" + 上傳筆數);
 	}
 
@@ -115,6 +105,7 @@ public class 構形資料庫轉至組建資料庫
 				註標++;
 			部件數++;
 		}
+		// System.err.println(部件數);
 		StringBuilder 構字式 = new StringBuilder();
 		註標 = 0;
 		while (註標 < 部件序控制碼.length)
@@ -149,7 +140,7 @@ public class 構形資料庫轉至組建資料庫
 	public StringBuilder 展開方便符號(int 控制碼, char[] 部件) throws IOException
 	{
 		StringBuilder 構字式 = new StringBuilder();
-		int 方便符號編號 = 方便符號控制碼[0] - 控制碼;
+		int 方便符號編號 = 控制碼 - 方便符號控制碼[0];
 		switch (方便符號編號)
 		{
 		case 0:
@@ -211,7 +202,7 @@ public class 構形資料庫轉至組建資料庫
 			構字式.append(部件);
 			break;
 		default:
-			throw new IOException();
+			throw new IOException("");
 		}
 		return 構字式;
 	}

8621b83abc62b3105b0978c00d8beee81db353b9
Ihc <ihcaoe@gmail.com>
2012-11-21 02:00:56 +0000

    之前做的，今天整理並上傳 丞宏

---
+++ src/cc/test/tool/DomCdpToPg.java
@@ -0,0 +1,69 @@
+package cc.test.tool;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * 用Dom來處理微軟Access產生的xml檔，使之可以轉成資料庫的xml格式。但其實作方法適合隨機存取不適合大型檔案。
+ * 
+ * @author Ihc
+ */
+public class DomCdpToPg
+{
+
+	/**
+	 * @param args
+	 *            程式參數
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			File file = new File("/home/Ihc/tmpfs/aa.xml");
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+			System.out.println("Root element "
+					+ doc.getDocumentElement().getNodeName());
+			NodeList nodeLst = doc.getElementsByTagName("檢字表");
+			System.out.println("Information of all employees");
+
+			for (int s = 0; s < nodeLst.getLength(); s++)
+			{
+				Node fstNode = nodeLst.item(s);
+				if (fstNode.getNodeType() == Node.ELEMENT_NODE)
+				{
+					Element fstElmnt = (Element) fstNode;
+					System.out.println(fstElmnt.getTextContent());
+					System.out.println(fstElmnt.getTagName());
+					System.out.println(fstElmnt.getElementsByTagName("*")
+							.getLength());
+					NodeList attr = fstElmnt.getElementsByTagName("*");
+					for (int i = 0; i < attr.getLength(); ++i)
+					{
+						System.out.println(fstElmnt.getElementsByTagName("*")
+								.item(i).getNodeName());
+						System.out.println(fstElmnt.getElementsByTagName("*")
+								.item(i).getTextContent());
+					}
+					System.out.println("----------");
+				}
+
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/cc/test/tool/StaxMsToPg.java
@@ -0,0 +1,111 @@
+package cc.test.tool;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamReader;
+
+/**
+ * 用Stax來處理微軟Access產生的xml檔，使之可以轉成資料庫的xml格式。其循序存取符合實際需要。
+ * 
+ * @author Ihc
+ */
+public class StaxMsToPg
+{
+
+	/**
+	 * @param args
+	 *            程式參數
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			File inputFile = new File("/home/Ihc/tmpfs/檢字表.xml");
+			PrintStream printStream = new PrintStream(new File(
+					inputFile.getParent() + "/output.xml"));
+			// printStream=printStream;
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(new FileInputStream(inputFile)));
+			printStream.println(bufferedReader.readLine());
+
+			BufferedInputStream bufferedInputStream = new BufferedInputStream(
+					new FileInputStream(inputFile));
+			InputStream in = bufferedInputStream;
+			XMLInputFactory factory = XMLInputFactory.newInstance();
+			XMLStreamReader parser = factory.createXMLStreamReader(in);
+			int level = 0;
+			boolean running = true;
+			while (running)
+			{
+				int event = parser.next();
+				switch (event)
+				{
+				case XMLStreamConstants.END_DOCUMENT:
+					parser.close();
+					running = false;
+					break;
+				case XMLStreamConstants.START_ELEMENT:
+					level++;
+					printStream.print('<');
+					if (level == 3)
+					{
+						printStream.print("column name=\""
+								+ parser.getLocalName() + "\"");
+					}
+					else
+					{
+						printStream.print(getTag(parser.getLocalName(), level));
+					}
+					printStream.print('>');
+					if (level == 1)
+						printStream.print("\n<records>");
+					break;
+				case XMLStreamConstants.END_ELEMENT:
+					if (level == 1)
+						printStream.println("</records>");
+					printStream.print("</");
+					printStream.print(getTag(parser.getLocalName(), level));
+					printStream.print('>');
+					level--;
+					break;
+				case XMLStreamConstants.CHARACTERS:
+					// case XMLStreamConstants.CDATA:
+					printStream.print(parser.getText());
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * @param origin
+	 *            原本標籤
+	 * @param level
+	 *            標籤在xml第幾層
+	 * @return 新標籤名
+	 */
+	static private String getTag(String origin, int level)
+	{
+		if (level == 2)
+			return "row";
+		if (level == 3)
+			return "column";
+		if (origin.equals("dataroot"))
+			return "data";
+		return origin;
+	}
+}

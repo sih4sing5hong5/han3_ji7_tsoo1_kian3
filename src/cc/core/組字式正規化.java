2007673c346ddf9da979f43167269b893a1930a0
Ihc <ihcaoe@gmail.com>
2012-12-22 10:23:03 +0000

    轉來轉去的正規化～～ 丞宏

---
+++ src/cc/core/組字式正規化.java
@@ -0,0 +1,45 @@
+package cc.core;
+
+/**
+ * 僅限雙組合符號的正規化型態。
+ * 
+ * @author Ihc
+ */
+public class 組字式正規化
+{
+	/**
+	 * 正規化部件結構。
+	 * 
+	 * @param 部件
+	 *            欲正規化的物件
+	 */
+	void 正規化(ChineseCharacter 部件)
+	{
+		if (ChineseCharacterTzuCombinationType.isCombinationType(部件
+				.getCodePoint()))
+		{
+			ChineseCharacterTzu 字部件 = (ChineseCharacterTzu) 部件;
+			if (字部件.getChildren().length == 2)
+			{
+				ChineseCharacter 倒爿部件 = 字部件.getChildren()[0];
+				while (倒爿部件.getCodePoint() == 字部件.getCodePoint())
+				{
+					ChineseCharacterTzu 倒爿字部件 = (ChineseCharacterTzu) 倒爿部件;
+					ChineseCharacter 倒倒爿部件 = 倒爿字部件.getChildren()[0], 倒正爿部件 = 倒爿字部件
+							.getChildren()[1], 正爿部件 = 字部件.getChildren()[1];
+					字部件.getChildren()[0] = 倒倒爿部件;
+					字部件.getChildren()[1] = 倒爿部件;
+					倒爿字部件.getChildren()[0] = 倒正爿部件;
+					倒爿字部件.getChildren()[1] = 正爿部件;
+				}
+			}
+			else
+			{
+				System.out.println("有三个以上的部件組合符號！！");
+			}
+			for (int i = 0; i < 字部件.getChildren().length; ++i)
+				正規化(字部件.getChildren()[i]);
+		}
+		return;
+	}
+}

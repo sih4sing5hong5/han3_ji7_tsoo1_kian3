fc1a1474a5a85cc3fb3c54c5cf2158b8ed1d8095
Ihc <ihcaoe@gmail.com>
2013-01-10 11:25:38 +0000

    模組化，閣加正規化的設定 丞宏

--- src/cc/example/awt/AwtTestExample.java
+++ src/cc/example/awt/AwtTestExample.java
@@ -6,14 +6,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Stroke;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.font.FontRenderContext;
 import java.awt.geom.AffineTransform;
 import java.util.Vector;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import cc.adjusting.bolder.FunctinoalBasicBolder;
 import cc.adjusting.bolder.NullStroke;
@@ -21,6 +16,9 @@ import cc.adjusting.piece.MergePieceAdjuster;
 import cc.core.ChineseCharacter;
 import cc.core.ChineseCharacterUtility;
 import cc.core.漢字序列分析工具;
+import cc.core.組字式部件;
+import cc.core.組字式部件正規化;
+import cc.core.組字式部件組字式建立工具;
 import cc.core.資料庫連線展開式查詢;
 import cc.moveable_type.ChineseCharacterMovableType;
 import cc.moveable_type.piece.PieceMovableType;
@@ -40,40 +38,26 @@ import cc.setting.piece.MergePieceSetter;
  * 
  * @author Ihc
  */
-public class AwtTestExample extends JPanel
+public class AwtTestExample extends Awt測試樣板
 {
 	/** 序列化編號 */
 	private static final long serialVersionUID = 1L;
-	/** 視窗寬度 */
-	protected static final int WIDTH = 1440;
-	/** 視窗高度 */
-	protected static final int HEIGHT = 1050;
-	/** 字型大小 */
-	static final int TYPE_SIZE = 75;
-	/** 每行字數 */
-	static final int LINE_SIZE = 12;
-	/** 測試漢字 */
-	static private final String word = /* "    ⿰禾火秋⿰⿰火牙阝"; */"一百二十三尺"
-			+ "秋漿國變務森" + "⿰禾火⿱將水⿴囗或" + "⿱⿰⿰糹言糹攵⿰矛⿱攵力⿱木⿰木木"
-			+ "⿱立⿱日十⿱⿱立日十章輻⿰車⿱一⿱口田⿰⿱十⿱田十⿱一⿱口田" + "⿱口⿰口口⿱夕夕，"
-			+ "⿴辶⿴宀⿱珤⿰隹⿰貝招⿴辶咼辶" + "⿴冖儿⿴冖几宀寶建⿴廴聿"
-			+ "⿴厂猒⿴广夏⿴疒丙⿴尸古⿴戶方⿴户方⿴户 ⿱户　⿰户　" + "⿴气亥⿴气乃⿴气亞"
-			+ "⿴冂⿱一口⿴冂⿱儿口⿴門一⿴門日⿴門月⿴鬥月⿴鬥市" + "⿱⿴乃又皿盈孕⿴乃子⿱乃子"
-			+ "⿴凵乂⿴凵⿰幺⿰丨幺⿴凵⿰豕⿰丨豕" + "⿴勹日⿰⿴弓土畺⿴匚甲⿴⼖⿱口⿰口口" + "繞熱然煎焦烹煦無勳点狗狚";// */;
-	/** 全字庫正宋體 */
-	static final String 全字庫正宋體 = "全字庫正宋體";
-	/** 全字庫正楷體 */
-	static final String 全字庫正楷體 = "全字庫正楷體";
-	/** 文泉驛正黑 */
-	static final String 文泉驛正黑 = "文泉驛正黑";
-	/** 文鼎中圓 */
-	static final String 文鼎中圓 = "文鼎中圓";
-	/** 超研澤中圓 */
-	static final String 超研澤中圓 = "超研澤中圓";
 	/** 測試用字體 */
-	static private final String FontName = 全字庫正宋體;
+	static final String 測試字體 = 全字庫正宋體;
 	/** 測試用屬性 */
-	static private final int FontStyle = Font.BOLD/* 0;// */;
+	static final int 測試屬性 = Font.BOLD/* 0;// */;
+
+	/**
+	 * 主函式，設定相關視窗資訊。
+	 * 
+	 * @param args
+	 *            呼叫引數
+	 */
+	public static void main(String[] args)
+	{
+		run(new AwtTestExample());
+		return;
+	}
 
 	@Override
 	public void paint(Graphics g1)
@@ -91,10 +75,24 @@ public class AwtTestExample extends JPanel
 		// TODO 資料庫連線展開式查詢() 展開式免查詢()
 		Vector<ChineseCharacter> ccArray = ccUtility.parseText();
 
+		組字式部件正規化 正規化工具 = new 組字式部件正規化();
+		組字式部件組字式建立工具 組字式建立工具 = new 組字式部件組字式建立工具();
+		for (ChineseCharacter 部件 : ccArray)
+		{
+//			System.out.println("XD");
+			組字式部件 組字部件 = (組字式部件) 部件;
+			組字部件.建立組字式(組字式建立工具);
+			System.out.println(組字部件.提到組字式());
+			正規化工具.正規化(部件);
+//			System.out.println("XD1");
+			組字部件.建立組字式(組字式建立工具);
+			System.out.println(組字部件.提到組字式());
+		}
+
 		System.out.println("設定中～～ 時間：" + System.currentTimeMillis());
 		ChineseCharacterTypeSetter setter = new MergePieceSetter(
-				FontName,
-				FontStyle,
+				測試字體,
+				測試屬性,
 				TYPE_SIZE,
 				new FontRenderContext(new AffineTransform(),
 						java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_DEFAULT,
@@ -131,44 +129,5 @@ public class AwtTestExample extends JPanel
 		System.out.println("結束了～～ 時間：" + System.currentTimeMillis());
 		System.out.println();
 		return;
-	}
-
-	/**
-	 * 主函式，設定相關視窗資訊。
-	 * 
-	 * @param args
-	 *            呼叫引數
-	 */
-	public static void main(String[] args)
-	{
-		JFrame f = new JFrame();
-		f.addWindowListener(new WindowAdapter()
-		{
-			public void windowClosing(WindowEvent e)
-			{
-				System.exit(0);
-			}
-		});
-		f.setContentPane(new AwtTestExample());
-		f.setSize(WIDTH, HEIGHT);
-		f.setVisible(true);
-	}
-
-	@Override
-	public String getName()
-	{
-		return "漢字測試";
-	}
-
-	@Override
-	public int getWidth()
-	{
-		return WIDTH;
-	}
-
-	@Override
-	public int getHeight()
-	{
-		return HEIGHT;
 	}
 }
---
+++ src/cc/example/awt/Awt測試樣板.java
@@ -0,0 +1,97 @@
+package cc.example.awt;
+
+import java.awt.Container;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * 測試的範例樣版。
+ * 
+ * @author Ihc
+ */
+abstract public class Awt測試樣板 extends JPanel
+{
+	/** 序列化編號 */
+	private static final long serialVersionUID = 1L;
+	/** 視窗寬度 */
+	protected static final int WIDTH = 1440;
+	/** 視窗高度 */
+	protected static final int HEIGHT = 1050;
+	/** 字型大小 */
+	static final int TYPE_SIZE = 75;
+	/** 每行字數 */
+	static final int LINE_SIZE = 12;
+	/** 測試漢字 */
+	static final String word = /* "    ⿰禾火秋⿰⿰火牙阝"; */"一百二十三尺" + "秋漿國變務森"
+			+ "⿰禾火⿱將水⿴囗或" + "⿱⿰⿰糹言糹攵⿰矛⿱攵力⿱木⿰木木"
+			+ "⿱立⿱日十⿱⿱立日十章輻⿰車⿱一⿱口田⿰⿱十⿱田十⿱一⿱口田" + "⿱口⿰口口⿱夕夕，"
+			+ "⿴辶⿴宀⿱珤⿰隹⿰貝招⿴辶咼辶" + "⿴冖儿⿴冖几宀寶建⿴廴聿"
+			+ "⿴厂猒⿴广夏⿴疒丙⿴尸古⿴戶方⿴户方⿴户 ⿱户　⿰户　" + "⿴气亥⿴气乃⿴气亞"
+			+ "⿴冂⿱一口⿴冂⿱儿口⿴門一⿴門日⿴門月⿴鬥月⿴鬥市" + "⿱⿴乃又皿盈孕⿴乃子⿱乃子"
+			+ "⿴凵乂⿴凵⿰幺⿰丨幺⿴凵⿰豕⿰丨豕" + "⿴勹日⿰⿴弓土畺⿴匚甲⿴⼖⿱口⿰口口" + "繞熱然煎焦烹煦無勳点狗狚";// */;
+	/** 全字庫正宋體 */
+	static final String 全字庫正宋體 = "全字庫正宋體";
+	/** 全字庫正楷體 */
+	static final String 全字庫正楷體 = "全字庫正楷體";
+	/** 文泉驛正黑 */
+	static final String 文泉驛正黑 = "文泉驛正黑";
+	/** 文鼎中圓 */
+	static final String 文鼎中圓 = "文鼎中圓";
+	/** 超研澤中圓 */
+	static final String 超研澤中圓 = "超研澤中圓";
+
+	/**
+	 * 主函式，設定相關視窗資訊。
+	 * 
+	 * @param args
+	 *            呼叫引數
+	 */
+	public static void main(String[] args)
+	{
+		run(null);
+		return;
+	}
+
+	/**
+	 * 範例型態視窗的設定佮產生。
+	 * 
+	 * @param 範例型態
+	 *            愛產生的範例型態
+	 */
+	protected static void run(Container 範例型態)
+	{
+		JFrame f = new JFrame();
+		f.addWindowListener(new WindowAdapter()
+		{
+			public void windowClosing(WindowEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		f.setContentPane(範例型態);
+		f.setSize(WIDTH, HEIGHT);
+		f.setVisible(true);
+
+	}
+
+	@Override
+	public String getName()
+	{
+		return "漢字測試";
+	}
+
+	@Override
+	public int getWidth()
+	{
+		return WIDTH;
+	}
+
+	@Override
+	public int getHeight()
+	{
+		return HEIGHT;
+	}
+}

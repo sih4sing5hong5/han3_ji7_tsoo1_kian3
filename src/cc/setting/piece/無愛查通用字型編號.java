922fc8456e2e5fbfc05d236c3be132acb573a8d2
Ihc <ihcaoe@gmail.com>
2013-04-01 09:10:20 +0000

    加一个無做代誌的介面 丞宏

---
+++ src/cc/setting/piece/無愛查通用字型編號.java
@@ -0,0 +1,23 @@
+package cc.setting.piece;
+
+/**
+ * 停止使用查通用字型編號的功能。
+ * 
+ * @author Ihc
+ */
+public class 無愛查通用字型編號 implements 展開式查通用字型編號
+{
+	/**
+	 * 提供介面，回傳揣無。
+	 * 
+	 * @param 展開式
+	 *            愛揣的字型展開式
+	 * @return 毋管按怎，攏回傳<code>null</code>
+	 */
+	@Override
+	public 通用字型號碼 查通用字型編號(String 展開式)
+	{
+		return null;
+	}
+
+}

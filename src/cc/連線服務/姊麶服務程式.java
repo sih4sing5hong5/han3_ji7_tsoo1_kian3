35663dcd674dc0541ec46f574d325e63cfbeb58b
Ihc <Ihc@gmail.com>
2013-07-12 16:04:13 +0800

    改本機監聽　丞宏

--- src/cc/連線服務/姊麶服務程式.java
+++ src/cc/連線服務/姊麶服務程式.java
@@ -1,6 +1,8 @@
 package cc.連線服務;
 
+import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
+import org.mortbay.jetty.nio.SelectChannelConnector;
 import org.mortbay.jetty.servlet.ServletHandler;
 
 /**
@@ -10,6 +12,9 @@ import org.mortbay.jetty.servlet.ServletHandler;
  */
 public class 姊麶服務程式
 {
+	/** 網路監聽埠口 */
+	static final int 連接埠 = 8080;
+
 	/**
 	 * 主要執行的函式。
 	 * 
@@ -18,26 +23,42 @@ public class 姊麶服務程式
 	 */
 	public static void main(String[] 參數)
 	{
-		Server server = new Server(8080);
+		Server server = new Server();
+		Connector connector = new SelectChannelConnector();
+		connector.setHost("localhost");
+		connector.setPort(連接埠);
+		server.addConnector(connector);
+
 		ServletHandler handler = new ServletHandler();
 		server.setHandler(handler);
 		handler.addServletWithMapping(組字服務.class, "/*");
-		try
+		boolean 遏袂啟動 = true;
+		while (遏袂啟動)
 		{
-			server.start();
+			try
+			{
+				server.start();
+				遏袂啟動 = false;
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				try
+				{
+					Thread.sleep(3000);
+				}
+				catch (InterruptedException e1)
+				{
+				}
+			}
 		}
-		catch (Exception e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println("服務啟動～～");
 		try
 		{
 			server.join();
 		}
 		catch (InterruptedException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}

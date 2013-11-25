a4a85d1e28e134efa00d22797a580cf3ed72fc88
Ihc <ihcaoe@gmail.com>
2013-11-25 10:39:22 +0800

    加著作權　丞宏

--- src/cc/example/reference/ControlPointsStroke.java
+++ src/cc/example/reference/ControlPointsStroke.java
@@ -1,5 +1,15 @@
 package cc.example.reference;
 
+/*
+ * Copyright (c) 2000 David Flanagan.  All rights reserved.
+ * This code is from the book Java Examples in a Nutshell, 2nd Edition.
+ * It is provided AS-IS, WITHOUT ANY WARRANTY either expressed or implied.
+ * You may study, use, and modify it for any non-commercial purpose.
+ * You may distribute it non-commercially as long as you retain this notice.
+ * For a commercial use license, or to purchase the book (recommended),
+ * visit http://www.davidflanagan.com/javaexamples2.
+ */
+
 import java.awt.BasicStroke;
 import java.awt.Shape;
 import java.awt.Stroke;
@@ -15,8 +25,7 @@ import java.awt.geom.PathIterator;
  * stroked shape.
  */
 
-public class ControlPointsStroke implements Stroke
-{
+public class ControlPointsStroke implements Stroke {
 	/** 控制點大小 */
 	float radius; // how big the control point markers should be
 
@@ -26,14 +35,12 @@ public class ControlPointsStroke implements Stroke
 	 * @param radius
 	 *            控制點大小
 	 */
-	public ControlPointsStroke(float radius)
-	{
+	public ControlPointsStroke(float radius) {
 		this.radius = radius;
 	}
 
 	@Override
-	public Shape createStrokedShape(Shape shape)
-	{
+	public Shape createStrokedShape(Shape shape) {
 		// Start off by stroking the shape with a thin line. Store the
 		// resulting shape in a GeneralPath object so we can add to it.
 		GeneralPath strokedShape = new GeneralPath(
@@ -45,11 +52,9 @@ public class ControlPointsStroke implements Stroke
 		float[] coords = new float[6];
 		int cnt = 0;
 		for (PathIterator i = shape.getPathIterator(null); !i.isDone(); i
-				.next())
-		{
+				.next()) {
 			int type = i.currentSegment(coords);
-			switch (type)
-			{
+			switch (type) {
 			case PathIterator.SEG_CUBICTO:
 				markPoint(strokedShape, coords[4], coords[5]); // falls through
 			case PathIterator.SEG_QUADTO:
@@ -77,8 +82,7 @@ public class ControlPointsStroke implements Stroke
 	 *            垂直位置
 	 */
 
-	void markPoint(GeneralPath path, float x, float y)
-	{
+	void markPoint(GeneralPath path, float x, float y) {
 		path.moveTo(x - radius, y - radius); // Begin a new sub-path
 		path.lineTo(x + radius, y - radius); // Add a line segment to it
 		path.lineTo(x + radius, y + radius); // Add a second line segment

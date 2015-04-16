67c7df2e3eb86f971b1eda52491a98a9596a7aee
薛丞宏 <ihcaoe@gmail.com>
2015-04-16 12:41:43 +0800

    把排版工具的轉活字陣列功能分開 丞宏

---
+++ 試驗/漢字組建/定版工具/MergePieceAdjuster_format試驗.java
@@ -0,0 +1,22 @@
+package 漢字組建.定版工具;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MergePieceAdjuster_format試驗
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}

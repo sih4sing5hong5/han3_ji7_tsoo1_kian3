4af15f666d9c0af7fcd7ad54213b5101cc0cc04a
薛丞宏 <ihcaoe@gmail.com>
2015-04-16 12:08:07 +0800

    先在試驗分類程式功能 丞宏

---
+++ 試驗/漢字組建/揀字工具/SimplePieceSetter試驗.java
@@ -0,0 +1,22 @@
+package 漢字組建.揀字工具;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimplePieceSetter試驗
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
---
+++ 試驗/漢字組建/筆觸工具/分離活字加粗試驗.java
@@ -0,0 +1,22 @@
+package 漢字組建.筆觸工具;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class 分離活字加粗試驗
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}

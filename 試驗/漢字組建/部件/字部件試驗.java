48ca514feaa632b0e3266779c12e8919220338bf
薛丞宏 <ihcaoe@gmail.com>
2015-04-19 20:03:00 +0800

    做好部件試驗 #7 #12

--- 試驗/漢字組建/部件/字部件試驗.java
+++ 試驗/漢字組建/部件/字部件試驗.java
@@ -2,21 +2,65 @@ package 漢字組建.部件;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class 字部件試驗
 {
-
-	@Before
-	public void setUp() throws Exception
+	@Test
+	public void 是文部件()
 	{
+		字部件 部件左右 = 字部件("⿰");
+		assertFalse(部件左右.是文部件());
 	}
 
 	@Test
-	public void test()
+	public void 是字部件()
 	{
-		fail("Not yet implemented");
+		字部件 部件左右 = 字部件("⿰");
+		assertTrue(部件左右.是文部件());
 	}
 
+	@Test
+	public void Unicode編號()
+	{
+		字部件 部件左右 = 字部件("⿰");
+		assertEquals("⿰".codePointAt(0), 部件左右.Unicode編號());
+	}
+
+	@Test
+	public void 部件組字式()
+	{
+		字部件 部件左右 = 字部件("⿰");
+		assertEquals("⿰", 部件左右.部件組字式());
+	}
+
+	@Test
+	public void 底下二元素()
+	{
+		字部件 部件左右 = 字部件("⿰");
+		assertEquals(2, 部件左右.底下元素().length);
+	}
+
+	@Test
+	public void 底下三元素()
+	{
+		字部件 部件左右 = 字部件("⿲");
+		assertEquals(3, 部件左右.底下元素().length);
+	}
+
+	@Test
+	public void 組合方式()
+	{
+		字部件 部件左右 = 字部件("⿰");
+		assertEquals(組合方式.左右合併, 部件左右.組合方式());
+	}
+
+	@Test
+	public void 樹狀結構組字式()
+	{
+		字部件 部件左右 = 字部件("⿰");
+		部件左右.底下元素()[0] = 文部件("");
+		部件左右.底下元素()[1] = 文部件("專");
+		assertEquals("⿰專", 部件左右.樹狀結構組字式());
+	}
 }
--- 試驗/漢字組建/部件/文部件試驗.java
+++ 試驗/漢字組建/部件/文部件試驗.java
@@ -7,16 +7,39 @@ import org.junit.Test;
 
 public class 文部件試驗
 {
-
-	@Before
-	public void setUp() throws Exception
+	@Test
+	public void 是文部件()
 	{
+		文部件 部件意 = 文部件("意");
+		assertTrue(部件意.是文部件());
 	}
 
 	@Test
-	public void test()
+	public void 是字部件()
 	{
-		fail("Not yet implemented");
+		文部件 部件意 = 文部件("意");
+		assertFalse(部件意.是文部件());
+	}
+
+	@Test
+	public void Unicode編號()
+	{
+		文部件 部件意 = 文部件("意");
+		assertEquals("意".codePointAt(0), 部件意.Unicode編號());
+	}
+
+	@Test
+	public void 部件組字式()
+	{
+		文部件 部件意 = 文部件("意");
+		assertEquals("意", 部件意.部件組字式());
+	}
+
+	@Test
+	public void 樹狀結構組字式()
+	{
+		文部件 部件意 = 文部件("意");
+		assertEquals("意", 部件意.樹狀結構組字式());
 	}
 
 }
